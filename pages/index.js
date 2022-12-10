eeed157d932207ed32f0bf133ff386d4b074a71b
kwaksj329 <kwaksj329@gmail.com>
2022-12-11 01:17:38 +0900

    2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Seo from "../components/Seo";
+import { useState, useEffect } from "react";
+
+const API_KEY = "6918df300e879c6f80b6018794e6c6b2";
 
 export default function Home() {
-  // const [counter, setCounter] = useState(0);
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(data.results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
