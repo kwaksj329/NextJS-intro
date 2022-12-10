6a428e7bed50a7b00cc942983b17f8fde9358d0e
kwaksj329 <kwaksj329@gmail.com>
2022-12-11 00:38:21 +0900

    2.0 Patterns

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function Home() {
   // const [counter, setCounter] = useState(0);
   return (
     <div>
+      <Seo title="Home" />
       <h1>Hello</h1>
-      {/* <h1>{counter}</h1> */}
-      {/* <button onClick={() => setCounter((prev) => prev + 1)}>+</button> */}
     </div>
   );
 }
